2aa8c264d9843b4501170281fb801734ed80f5cb
Keyvan Fatehi <keyvanfatehi@gmail.com>
2017-07-22 09:50:02 -0700

    fix up plugin.m per skypeweb example

--- adium/MXMatrixLibPurpleServicePlugin.m
+++ adium/MXMatrixLibPurpleServicePlugin.m
@@ -3,36 +3,38 @@
 //  AdiumMatrix
 //
 //  Based on microblog-purple created by jsippel on 1/7/09.
+//  Based on skypeweb4adium created by Tobias Tangemann on 26.04.15.
 //
 
-#import <Adium/AIPlugin.h>
-#import <AdiumLibpurple/AILibpurplePlugin.h>
+#import <Adium/ESDebugAILog.h>
 
 #import "MXMatrixLibPurpleServicePlugin.h"
 #import "MXMatrixService.h"
+#import "libmatrix.h"
+
+extern void purple_init_matrix_plugin();
+
 
 @implementation MXMatrixLibPurpleServicePlugin
 
-extern BOOL purple_init_plugin(void);
-
-
 - (void) installPlugin
 {
-    [MXMatrixService registerService];
     AILog(@"Installing Matrix Plugin");
-
+    purple_init_matrix_plugin();
+    [MXMatrixService registerService];
 }
-
 
 - (void)loadLibpurplePlugin
 {
-	AILog(@"Loading Matrix Plugin");
-    purple_init_matrix_plugin();
+    AILog(@"Loading Matrix Plugin %s (purple-matrix: "DISPLAY_VERSION")",[[self pluginVersion] UTF8String]);
 }
 
-- (void) installLibpurplePlugin
-{
-	AILog(@"Loading Matrix LibPurple Plugin");
-}
+- (NSString *)pluginAuthor      { return @"Keyvan Fatehi"; }
+- (NSString *)pluginDescription { return @"Matrix"; }
+- (NSString *)pluginURL         { return @"https://github.com/matrix-org/purple-matrix"; }
+- (NSString *)pluginVersion     { return [[[NSBundle bundleForClass:[self class]] infoDictionary] objectForKey:@"CFBundleVersion"]; }
+
+- (void)uninstallPlugin {}
+- (void)installLibpurplePlugin {}
 
 @end
